3df38773d5a6d114cb17df935f7cfa578a0c17fb
SLIMANI Imed Eddine Abderrahmane <imeds2000@gmail.com>
2025-08-12 18:36:20 +0100

    add production remote url to next config

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [new URL("http://localhost:8000/storage/**")],
+    remotePatterns: [
+      new URL("http://localhost:8000/storage/**"),
+      new URL("http://164.90.224.4/storage/**"),
+    ],
   },
   async redirects() {
     return [
